91fc44cb77123a0cff78a7d49d3106f5a1730ff1
Mark Heckler <mark.heckler@gmail.com>
2024-01-20 15:32:53 -0600

    Terraform enabled

---
+++ src/main/resources/terraform/main.tf
@@ -0,0 +1,91 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.88.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "mkheck" {
+  name     = var.AZ_RESOURCE_GROUP
+  location = var.AZ_LOCATION
+}
+
+# resource "azurerm_log_analytics_workspace" "mkheck" {
+#   name                = "acctest-01"
+#   location            = azurerm_resource_group.mkheck.location
+#   resource_group_name = azurerm_resource_group.mkheck.name
+#   sku                 = "PerGB2018"
+#   retention_in_days   = 30
+# }
+
+resource "azurerm_container_app_environment" "mkheck" {
+  name                = var.AZ_CONTAINERAPP_ENV
+  location            = azurerm_resource_group.mkheck.location
+  resource_group_name = azurerm_resource_group.mkheck.name
+  #   log_analytics_workspace_id = azurerm_log_analytics_workspace.mkheck.id
+}
+
+
+data "azurerm_container_registry" "acr" {
+  name                = var.ACR_NAME
+  resource_group_name = var.ACR_RESOURCE_GROUP
+}
+
+resource "azurerm_user_assigned_identity" "mkheck" {
+  location = azurerm_resource_group.mkheck.location
+  # name                = "mycontainerapp"
+  name                = var.ACR_MANAGED_IDENTITY
+  resource_group_name = azurerm_resource_group.mkheck.name
+  # identity_type       = "UserAssigned"
+  # identity_ids = [
+  #   data.azurerm_container_registry.acr.id
+  # ]
+}
+
+resource "azurerm_role_assignment" "mkheck" {
+  scope                = data.azurerm_container_registry.acr.id
+  role_definition_name = "acrpull"
+  principal_id         = azurerm_user_assigned_identity.mkheck.principal_id
+  depends_on = [
+    azurerm_user_assigned_identity.mkheck
+  ]
+}
+
+
+resource "azurerm_container_app" "mkheck" {
+  name                         = var.AZ_CONTAINERAPP_NAME
+  container_app_environment_id = azurerm_container_app_environment.mkheck.id
+  resource_group_name          = azurerm_resource_group.mkheck.name
+  # revision_mode                = "Single"
+  revision_mode = "Multiple"
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.mkheck.id]
+  }
+
+  registry {
+    # username = var.SP_USER_NAME
+    # password_secret_name = var.SP_PASSWORD
+    server = var.ACR_REGISTRY_SVR
+    # server   = data.azurerm_container_registry.acr.login_server # Where the hell did login_server come from?!?!
+    # server = data.azurerm_container_registry.acr.name
+    # identity = var.ACR_MANAGED_IDENTITY
+    identity = azurerm_user_assigned_identity.mkheck.id
+  }
+
+  template {
+    container {
+      # name   = "mycontainerapp"
+      name   = var.AZ_CONTAINERAPP_NAME
+      image  = "${var.ACR_REGISTRY_SVR}/${var.DOCKER_ID}/${var.IMAGE_NAME}:${var.IMAGE_TAG}"
+      cpu    = 0.5
+      memory = "1Gi"
+    }
+  }
+}
---
+++ src/main/resources/terraform/variables.tf
@@ -0,0 +1,72 @@
+variable "AZ_LOCATION" {
+  type     = string
+  nullable = false
+  default  = "eastus"
+}
+
+variable "AZ_RESOURCE_GROUP" {
+  type     = string
+  nullable = false
+}
+
+variable "AZ_CONTAINERAPP_ENV" {
+  type     = string
+  nullable = false
+}
+
+variable "AZ_CONTAINERAPP_NAME" {
+  type     = string
+  nullable = false
+}
+
+variable "ACR_NAME" {
+  type     = string
+  nullable = false
+}
+
+variable "ACR_RESOURCE_GROUP" {
+  type     = string
+  nullable = false
+}
+
+variable "ACR_REGISTRY_SVR" {
+  type     = string
+  nullable = false
+}
+
+variable "ACR_MANAGED_IDENTITY" {
+  type     = string
+  nullable = false
+}
+
+variable "SP_USER_NAME" {
+  type     = string
+  nullable = false
+}
+
+variable "SP_NAME" {
+  type     = string
+  nullable = false
+}
+
+variable "SP_PASSWORD" {
+  type     = string
+  nullable = false
+}
+
+variable "DOCKER_ID" {
+  type     = string
+  nullable = false
+  default  = "hecklerm"
+}
+
+variable "IMAGE_NAME" {
+  type     = string
+  nullable = false
+}
+
+variable "IMAGE_TAG" {
+  type     = string
+  nullable = false
+  default  = "0.0.1-SNAPSHOT"
+}
